3075d36413d4daf7da92dd942a5259b80997ee94
Ahmet Emre Tamam <aemretamam@gmail.com>
2023-03-02 17:25:38 +0300

    donusumler

---
+++ donusumler/Program.cs
@@ -0,0 +1,76 @@
+﻿namespace donusumler;
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Implicit Conversion (bilinçsiz dönüşüm)
+
+        byte a = 5;
+        sbyte b = 30;
+        short c = 10;
+
+        int d = a+b+c;
+        Console.WriteLine(d);
+
+        long h = d;
+        Console.WriteLine(h);
+
+        float i = h;
+        Console.WriteLine(i);
+
+        string e = "emre";
+        char f = 'a';
+        object g = e+f;
+        Console.WriteLine(g);
+
+        //Explicit Conversion (Bilinçli Dönüşüm)
+
+        int x = 4;
+        byte y = (byte)x;
+        Console.WriteLine(y);
+
+        int z = 100;
+        byte t = (byte)z;
+        Console.WriteLine(t);
+
+        float w = 10.3f;
+        byte v = (byte)w;
+        Console.WriteLine(w);
+
+        // ToString Method
+
+        int xx = 6;
+        string yy = xx.ToString();
+        Console.WriteLine(yy);
+
+        // System.Convert
+
+        string s1 = "10", s2 = "20";
+        int sayi1, sayi2;
+        int toplam;
+
+        sayi1 = Convert.ToInt32(s1);
+        sayi2 = Convert.ToInt32(s2);
+
+        toplam = sayi1 + sayi2;
+        Console.WriteLine(toplam);
+
+        // Parse
+
+        ParseMethod();
+    }
+
+    public static void ParseMethod()
+    {
+        string metin1 = "10";
+        string metin2 = "10.25";
+        int rakam;
+        double dble;
+
+        rakam = Int32.Parse(metin1); //parse her zaman string dönüştürür.
+        dble = Double.Parse(metin2);
+
+        Console.WriteLine(rakam);
+        Console.WriteLine(dble);
+    }
+}
