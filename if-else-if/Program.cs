aa45401c8aa2e4dba61bbf1c73f71925b6e923d5
Ahmet Emre Tamam <aemretamam@gmail.com>
2023-03-02 18:57:08 +0300

    if-else

---
+++ if-else-if/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace if_else_if;
+class Program
+{
+    static void Main(string[] args)
+    {
+        int time = DateTime.Now.Hour;
+
+        // if * else if
+
+        if(time >= 6 && time <= 10)
+            Console.WriteLine("Günaydin!");
+        else if(time <= 18)
+            Console.WriteLine("İyi Günler!");
+        else
+            Console.WriteLine("İyi Geceler!");
+
+        // ternary if * ? true : false
+        string sonuc;
+        sonuc = time >= 6 && time <= 10 ? "Günaydın" : time <= 18 ? "İyi Günler" : "İyi Geceler";
+        Console.WriteLine(sonuc);
+    }
+}
