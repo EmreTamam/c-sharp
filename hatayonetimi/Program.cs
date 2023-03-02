6d2976a761e3fe9d6c930f31a77eaf26100d165c
Ahmet Emre Tamam <aemretamam@gmail.com>
2023-03-02 18:31:37 +0300

    HataYönetimi

---
+++ hatayonetimi/Program.cs
@@ -0,0 +1,45 @@
+﻿namespace hatayonetimi;
+class Program
+{
+    static void Main(string[] args)
+    {
+        try
+        {
+        Console.WriteLine("Sayı Giriniz!:");
+        int sayi = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Girdiğiniz Sayi:" + sayi);
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("Hata: " + ex.Message.ToString());
+        }
+        finally
+        {
+            Console.WriteLine("Ok.");
+        }
+
+        try
+        {
+            int a = int.Parse(null);
+        }
+        catch (ArgumentNullException ex)
+        {
+            Console.WriteLine("Boş Deger Girdiniz.");
+            Console.WriteLine(ex);
+        }
+        catch (FormatException ex)
+        {
+            Console.WriteLine("Veri uygunsuz.");
+            Console.WriteLine(ex);
+        }
+        catch (OverflowException ex)
+        {
+            Console.WriteLine("Çok büyük yada küçük bir değer girildi.");
+            Console.WriteLine(ex);
+        }
+        finally
+        {
+            Console.WriteLine("Ok.");
+        }
+    }
+}
