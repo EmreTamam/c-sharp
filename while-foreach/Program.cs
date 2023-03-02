38cb28ba736596b25e6065d7f98b172e9c17938e
Ahmet Emre Tamam <aemretamam@gmail.com>
2023-03-02 20:14:15 +0300

    while-foreach

---
+++ while-foreach/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace while_foreach;
+class Program
+{
+    static void Main(string[] args)
+    {
+        //while
+        int sayi = int.Parse(Console.ReadLine());
+        int sayac = 1;
+        int toplam = 0;
+        while (sayac <= sayi)
+        {
+            toplam += sayac;
+            sayac++;
+        }
+        Console.WriteLine(toplam/sayi);
+
+        char character = 'a';
+        while (character <= 'z')
+        {
+            Console.Write(character);
+            character++;
+        }
+
+        //foreach
+        string[] arabalar = {"BMW","FORD","TOYOTA"};
+        foreach (var araba in arabalar)
+        {
+            Console.WriteLine(araba);
+        }
+    }
+}
