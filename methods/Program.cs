c9f42835c8cb8a892d117805afe1703e32c24fee
Ahmet Emre Tamam <aemretamam@gmail.com>
2023-03-05 20:32:14 +0300

    methods

---
+++ methods/Program.cs
@@ -0,0 +1,40 @@
+﻿namespace methods;
+class Program
+{
+    static void Main(string[] args)
+    {
+        // erisim_belirteci geri_donüstipi metot_adi(parametreListesi/argüman)
+
+        int a = 2;
+        int b = 3;
+
+        int sonuc = Topla(a, b);
+        Console.WriteLine(sonuc);
+
+        Metotlar inst = new Metotlar();
+        inst.EkranaYazdir(Convert.ToString(sonuc));
+
+        int sonuc2 = inst.ArttirVeTopla(ref a, ref b);
+        inst.EkranaYazdir(Convert.ToString(sonuc2));
+    }
+
+    static int Topla(int x, int y)
+    {
+        return(x+y);
+    }
+}
+
+class Metotlar
+{
+    public void EkranaYazdir(string veri)
+    {
+        Console.WriteLine(veri);
+    }
+
+    public int ArttirVeTopla(ref int x, ref int y)
+    {
+        x += 1;
+        y += 1;
+        return x + y;
+    }
+}
