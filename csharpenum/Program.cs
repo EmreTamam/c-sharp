c754fc3d5797b27a3763e3ff9b752b999d90c601
Ahmet Emre Tamam <aemretamam@gmail.com>
2023-03-20 19:34:08 +0300

    c

---
+++ csharpenum/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace csharpenum;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine(Gunler.Pazartesi);
+        Console.WriteLine((int)Gunler.Cuma);
+
+        int sicaklik = 25;
+
+        if(sicaklik <= (int)HavaDurumu.Normal)
+            Console.WriteLine("Hava Soğuk");
+        else if(sicaklik >= (int)HavaDurumu.Normal && sicaklik <= (int)HavaDurumu.Sıcak)
+            Console.WriteLine("Hava Normal");
+        else if(sicaklik >= (int)HavaDurumu.Sıcak)
+            Console.WriteLine("Hava Sıcak");
+
+            
+    }
+}
+
+enum Gunler
+{
+    Pazartesi = 1,
+    Sali,
+    Carsamba,
+    Persembe,
+    Cuma,
+    Cumartesi,
+    Pazar
+}
+
+enum HavaDurumu
+{
+    Soğuk = 5,
+    Normal = 20,
+    Sıcak = 30
+}
---
+++ struct/Program.cs
@@ -0,0 +1,40 @@
+﻿namespace structer;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Dikdörtgen dk = new Dikdörtgen();
+        dk.kisaKenar = 5;
+        dk.uzunKenar = 7;
+
+        Console.WriteLine("Alan: {0}", dk.AlanHesapla());
+
+        Dikdörtgen_Struct dkstruct;
+        dkstruct.kisaKenar = 5;
+        dkstruct.uzunKenar = 7;
+
+        Console.WriteLine("Alan: {0}", dkstruct.AlanHesapla());
+    }
+}
+
+class Dikdörtgen
+{
+    public int kisaKenar;
+    public int uzunKenar;
+
+    public long AlanHesapla()
+    {
+        return this.kisaKenar * this.uzunKenar;
+    }
+}
+
+struct Dikdörtgen_Struct
+{
+    public int kisaKenar;
+    public int uzunKenar;
+
+    public long AlanHesapla()
+    {
+        return this.kisaKenar * this.uzunKenar;
+    }
+}
